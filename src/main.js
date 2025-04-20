72f098bb9e6de2121e4e2fcfbfd7791917f6aed5
Carmen Fregoso Rojas <76757516+cafrmen@users.noreply.github.com>
2025-04-19 20:04:36 -0600

    Update main.js

--- src/main.js
+++ src/main.js
@@ -60,7 +60,7 @@ const soundArtist = (e) => {
     insidePortrait.style.setProperty('border-radius', '5px');
 
     boxes.forEach(box => {
-        box.style.setProperty('background-image', 'url("../media/artist.png")');
+        box.style.setProperty('background-image', 'url("portfolio/media/artist.png")');
         box.style.setProperty('background-size', '233%');
         box.style.setProperty('border', '5px solid #4C0033');
         box.style.setProperty('border-radius', '5%');
@@ -80,7 +80,7 @@ const granulator = (e) => {
     insidePortrait.style.setProperty('border-radius', '8px');
 
     boxes.forEach(box => {
-        box.style.setProperty('background-image', 'url("../media/granular.png")');
+        box.style.setProperty('background-image', 'url("cafrmen.github.io/portfolio/media/granular.png")');
         box.style.setProperty('background-size', '233%');
         box.style.setProperty('border', '8px solid black');
         box.style.setProperty('border-radius', '8px');
@@ -100,7 +100,7 @@ const nasaPost = (e) => {
     insidePortrait.style.setProperty('border-radius', '5px');
 
     boxes.forEach(box => {
-        box.style.setProperty('background-image', 'url("../media/nasa.png")');
+        box.style.setProperty('background-image', 'url("https://cafrmen.github.io/portfolio/media/nasa.png")');
         box.style.setProperty('background-size', '233%');
         box.style.setProperty('border', '5px solid #FF4545');
         box.style.setProperty('border-radius', '5px');
